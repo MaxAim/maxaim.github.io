d711a9615ffa3ba40930b712773be4bf9e24c9ab
MaxAim <75140148+MaxAim@users.noreply.github.com>
2021-02-03 22:32:33 -0300

    Add files via upload

--- js/desafio.js
+++ js/desafio.js
@@ -5,27 +5,65 @@ function Producto(nombre, precio, modelo, tipo, numero){
 	this.numero = numero;
 	this.tipo = tipo;
 }
-var kimetsu = new Producto("Kimetsu movie book", 1000, "Especial", "Libro", "1")
-var kimetsu2 = new Producto("Parca Kimetsu no yaiba", 6000, "L", "Ropa", "2")
-var onePiece = new Producto("Llavero One piece", 200, "Unico", "Llavero", "3")
-var onePiece2 = new Producto("Funko Pop Chopper One Piece", 3000, "Unico", "Funko Pop", "4")
-var onePiece3 = new Producto("Llavero y tsuka para llave Kimetsu", 400, "Unico", "Llavero", "5")
-var pocky = new Producto("Pocky", 290, "50g", "Comida", "6")
-var kracie = new Producto("Kracie Popin’ Cookin' Animal Pancake", 490, "Unico", "Comida", "7")
-var kracie2 = new Producto("Kracie Popin' Cookin' Ice Cream Cake ", 460, "Unico", "Comida", "8")
-var kimetsu3 = new Producto("Nendoroid Nezuko Kamado", 6000, "Unico", "Nendroid", "9")
-var jojo = new Producto("Llavero mascara JoJo", 600, "Unico", "Llavero", "10")
+var kimetsu = new Producto("Kimetsu movie book", 1000, "Especial", "Libro", 1)
+var kimetsu2 = new Producto("Parca Kimetsu no yaiba", 6000, "L", "Ropa", 2)
+var onePiece = new Producto("Llavero One piece", 200, "Unico", "Llavero", 3)
+var onePiece2 = new Producto("Funko Pop Chopper One Piece", 3000, "Unico", "Funko Pop", 4)
+var onePiece3 = new Producto("Llavero y tsuka para llave Kimetsu", 400, "Unico", "Llavero", 5)
+var pocky = new Producto("Pocky", 290, "50g", "Comida", 6)
+var kracie = new Producto("Kracie Popin’ Cookin' Animal Pancake", 490, "Unico", "Comida", 7)
+var kracie2 = new Producto("Kracie Popin' Cookin' Ice Cream Cake ", 460, "Unico", "Comida", 8)
+var kimetsu3 = new Producto("Nendoroid Nezuko Kamado", 6000, "Unico", "Nendroid", 9)
+var jojo = new Producto("Llavero mascara JoJo", 600, "Unico", "Llavero", 10)
 var list = [0, kimetsu, kimetsu2, onePiece, onePiece2, onePiece3, pocky, kracie, kracie2, kimetsu3, jojo]
 var carrito = []
 var total = 0
+var storage = localStorage
 
 function add(id){
+	if(id in storage){
+		storage[id] = parseInt(storage[id]) + 1
+	}
+	else{
+		storage.setItem(id, 1);
+	}
 	var btn = document.getElementById("btn-" + id);
-	btn.remove()
-	total = total + list[id].precio
-	carrito.push(list[id].nombre)
+	btn.innerHTML = "Agregar(" + parseInt(storage[id]) + " en carrito)";
 }
+
 function calcular(){
-	var final = (carrito.join("\n\n"))
-	alert(final + "\n\n" + "El total seria de ¥" + total)
+	for (const [key, value] of Object.entries(storage)){
+		carrito.push(list[key].nombre + " x" + storage[key]);
+	}
+	for (const [key, value] of Object.entries(storage)){
+		sub = parseInt(list[key].precio * storage[key])
+		total = total + sub;
+	}
+	final = (carrito.join("<br>"))
+	var crt = document.getElementById("crt");
+	crt.innerHTML = final
+	var ttl = document.getElementById("ttl");
+	ttl.innerHTML = "El total seria de ¥" + total
+	carrito = []
+	total = 0
 }
+
+function vaciar(){
+	localStorage.clear()
+	crt.innerHTML = "Nada aun"
+	ttl.innerHTML = "El total seria de ¥0"
+	for (var i = list.length - 1; i > 0; i--) {
+		btn = document.getElementById("btn-" + [i])
+		btn.innerHTML = "Agregar"
+	}
+}
+
+
+//	final = (carrito.join("\n\n"))
+//	var ctr = document.getElementById(htmlcarrito)
+//	ctr.innerHTML = final + "\n\n"
+//	var ttl = document.getElementById(total)
+//	txtttl = "El total seria de ¥" + total
+//	ttl.innerHTML = txtttl
+//	total = 0
+//	carrito = []
