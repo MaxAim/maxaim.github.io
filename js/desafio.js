cd7fe83f31d40679b07fc7ec4dde43d2f74a7c5c
MaxAim <75140148+MaxAim@users.noreply.github.com>
2021-02-28 19:08:16 -0300

    Add files via upload

--- js/desafio.js
+++ js/desafio.js
@@ -69,3 +69,14 @@ $(function(){
 		},
 	})
 });
+
+
+$(document).mouseup(function(e) 
+{
+    var container = $(".carrito-dropdown");
+
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        container.hide();
+    }
+});
